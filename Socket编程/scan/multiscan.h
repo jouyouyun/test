f390f1a24ef64e5ed22612a23b93db9b8ee3ce7c
jouyouyun <jouyouwen717@gmail.com>
2012-12-02 16:32:13 +0000

    shmem

--- Socket编程/scan/multiscan.h
+++
@@ -1,6 +0,0 @@
-#include <pthread.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-
-
--- Socket编程/scan/scanmodule.h
+++
@@ -1,7 +0,0 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <pthread.h>
-#include <stdlib.h>
-
-
